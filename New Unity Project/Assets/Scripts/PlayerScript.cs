a1c982664d21659350822f068f62f3f8dc1cd168
Hosang Yoo <you8147@naver.com>
2019-07-02 20:27:38 +0900

    이동 손보기 완료, 왼쪽 Shift키로 저속모드(통상 1/4속도) 구현

--- New Unity Project/Assets/Scripts/PlayerScript.cs
+++ New Unity Project/Assets/Scripts/PlayerScript.cs
@@ -8,18 +8,30 @@ public class PlayerScript : MonoBehaviour
     /// <summary>
     /// 1 - The speed of the ship
     /// </summary>
-    public Vector2 speed = new Vector2(50, 50);
+    public float speed = 18f;
 
     void Update()
     {
         // 3 - Retrieve axis information
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-        Vector2 destination = new Vector2(transform.position.x + (inputX*speed.x), transform.position.y + (inputY*speed.y));
+        float inputX = 0f;
+        float inputY = 0f;
+
+        //방향설정
+
+        if (Input.GetKey(KeyCode.LeftArrow)) inputX = -1f;
+        else if (Input.GetKey(KeyCode.RightArrow)) inputX = 1f;
+
+        if (Input.GetKey(KeyCode.DownArrow)) inputY = -1f;
+        else if (Input.GetKey(KeyCode.UpArrow)) inputY = 1f;
+
+        //저속모드
+
+        if (Input.GetKey(KeyCode.LeftShift)) { inputX /= 4; inputY /= 4; }
+
+        Vector2 destination = new Vector2(transform.position.x + (inputX * speed * Time.deltaTime), transform.position.y + (inputY * speed * Time.deltaTime));
 
         // 4 - Movement per direction
-        transform.position = Vector2.MoveTowards(transform.position, destination, Time.deltaTime);
-
+        transform.position = destination;
         // 5 - Shooting
         //bool shoot = Input.GetButtonDown("Fire1");
         // Careful: For Mac users, ctrl + arrow is a bad idea
@@ -32,5 +44,8 @@ public class PlayerScript : MonoBehaviour
         // false because the player is not an enemy
         //       weapon.Attack(false);
         //   }
+    }
+    void FixedUpdate() {
+
     }
 }
