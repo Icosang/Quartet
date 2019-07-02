7939747d2c902cb53485f25cf560f6ca2353cc5d
Hosang Yoo <you8147@naver.com>
2019-07-02 18:56:27 +0900

    배경, 플레이어 추가(이동 손봐야댐)

---
+++ New Unity Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Player controller and behavior
+/// </summary>
+public class PlayerScript : MonoBehaviour
+{
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
+    public Vector2 speed = new Vector2(50, 50);
+
+    void Update()
+    {
+        // 3 - Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        Vector2 destination = new Vector2(transform.position.x + (inputX*speed.x), transform.position.y + (inputY*speed.y));
+
+        // 4 - Movement per direction
+        transform.position = Vector2.MoveTowards(transform.position, destination, Time.deltaTime);
+
+        // 5 - Shooting
+        //bool shoot = Input.GetButtonDown("Fire1");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        //if (shoot)
+        //{
+        //   WeaponScript weapon = GetComponent<WeaponScript>();
+        //    if (weapon != null)
+        //    {
+        // false because the player is not an enemy
+        //       weapon.Attack(false);
+        //   }
+    }
+}
