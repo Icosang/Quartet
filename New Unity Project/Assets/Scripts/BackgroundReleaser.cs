9dc4cf207818cc9fa2043ff816302ad32d44a00f
Hosang Yoo <you8147@naver.com>
2019-07-05 23:54:50 +0900

    주인공 샷 스프라이트 바꿈, LinearShot.prefab 2개로 변경 완료 Z키 누를때만 발사되게, 화면 나가면 총알 해제되게 해야 함 주인공 샷 2개가 번갈아가면서 나가는 것도 해결해야 함

---
+++ New Unity Project/Assets/Scripts/BackgroundReleaser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundReleaser : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    { 
+        UbhBullet bullet = other.gameObject.transform.parent?.GetComponent<UbhBullet>();
+        if (bullet != null){
+            UbhObjectPool.instance.ReleaseBullet(bullet);
+        }
+    }
+}
