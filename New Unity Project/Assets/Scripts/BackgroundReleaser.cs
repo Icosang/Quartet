e3cbce4a7911d4b5ba935d65f9f4bff1b736881c
Hosang Yoo <you8147@naver.com>
2019-07-11 21:48:08 +0900

    BackgroundReleaser 개선, Player 소트레이어 추가, 모든 오브젝트 z값 0으로 수정

--- New Unity Project/Assets/Scripts/BackgroundReleaser.cs
+++ New Unity Project/Assets/Scripts/BackgroundReleaser.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundReleaser : MonoBehaviour
+
 {
-    void OnTriggerEnter(Collider other)
-    { 
-        UbhBullet bullet = other.gameObject.transform.parent?.GetComponent<UbhBullet>();
-        if (bullet != null){
-            UbhObjectPool.instance.ReleaseBullet(bullet);
+    void OnTriggerExit2D(Collider2D c)
+
+    {
+        //임시로 플레이어불렛만 적용, 나중에 고칠것
+        if (c.gameObject.tag == "Bullet")
+        {
+            UbhBullet bullet = null;
+
+            if (c.transform.parent.GetComponent<UbhPlayerBullet>() != null){
+                bullet = c.transform.parent.GetComponent<UbhPlayerBullet>();
+            }
+
+            if (bullet != null && bullet.isActive){
+                UbhObjectPool.instance.ReleaseBullet(bullet);
+            }
+
         }
+
     }
+
 }
