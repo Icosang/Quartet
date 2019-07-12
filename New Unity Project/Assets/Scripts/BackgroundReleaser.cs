a607254353235f28d254f9d81f6638e2e9ca5c41
Hosang Yoo <you8147@naver.com>
2019-07-12 17:48:35 +0900

    BackgroundReleaser 코드 롤백 바꾸면 안되는거였음

--- New Unity Project/Assets/Scripts/BackgroundReleaser.cs
+++ New Unity Project/Assets/Scripts/BackgroundReleaser.cs
@@ -9,7 +9,13 @@ public class BackgroundReleaser : MonoBehaviour
         //임시로 플레이어불렛만 적용, 나중에 고칠것
         if (c.gameObject.tag == "Bullet")
         {
-            UbhBullet bullet = c.transform.parent?.GetComponent<UbhPlayerBullet>();
+            UbhBullet bullet = null;
+
+            if (c.transform.parent.GetComponent<UbhPlayerBullet>() != null)
+            {
+                bullet = c.transform.parent.GetComponent<UbhPlayerBullet>();
+            }
+
             if (bullet != null && bullet.isActive){
                 UbhObjectPool.instance.ReleaseBullet(bullet);
             }
