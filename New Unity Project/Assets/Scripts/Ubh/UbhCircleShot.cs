2cea356b67ae67acd875d794ab0130ad389a3d82
Hosang Yoo <you8147@naver.com>
2019-07-31 20:22:50 +0900

    커밋 안되었던 것 추가

---
+++ New Unity Project/Assets/Scripts/Ubh/UbhCircleShot.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+/// <summary>
+/// Ubh circle shot.
+/// </summary>
+[AddComponentMenu("UniBulletHell/Shot Pattern/Circle Shot")]
+
+
+public class UbhCircleShot : UbhBaseShot
+{
+    [Header("===== CircleShot Settings =====")]
+    // "Set a angle of shot. (0 to 360)"
+    [Range(0f, 360f), FormerlySerializedAs("_Angle")]
+    public float m_angle = 0f;
+    public override void Shot()
+    {
+        if (m_bulletNum <= 0 || m_bulletSpeed <= 0f)
+        {
+            Debug.LogWarning("Cannot shot because BulletNum or BulletSpeed is not set.");
+            return;
+        }
+
+        if (m_shooting)
+        {
+            return;
+        }
+
+        m_shooting = true;
+    }
+
+    private void Update()
+    {
+        if (m_shooting == false)
+        {
+            return;
+        }
+
+        float shiftAngle = 360f / (float)m_bulletNum;
+
+        for (int i = 0; i < m_bulletNum; i++)
+        {
+            UbhBullet bullet = GetBullet(transform.position);
+            if (bullet == null)
+            {
+                break;
+            }
+
+            float angle = shiftAngle * i + m_angle;
+
+            ShotBullet(bullet, m_bulletSpeed, angle);
+        }
+
+        FiredShot();
+
+        FinishedShot();
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Ubh/UbhEnemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+public class UbhEnemy : UbhMonoBehaviour
+{
+    public const string NAME_PLAYER = "Player";
+    public const string NAME_PLAYER_BULLET = "PlayerBullet";
+
+    private const string ANIM_DAMAGE_TRIGGER = "Damage";
+
+    [SerializeField, FormerlySerializedAs("_Hp")]
+    private int m_hp = 1;
+    // 포인트. 나중에 시스템에 추가할 것
+    [SerializeField, FormerlySerializedAs("_Point")]
+    private int m_point = 100;
+    
+    private void OnTriggerEnter2D(Collider2D c)
+    {
+        // *It is compared with name in order to separate as Asset from project settings.
+        //  However, it is recommended to use Layer or Tag.
+        if (c.name.Contains(NAME_PLAYER_BULLET))
+        {
+            UbhPlayerBullet playerBullet = c.transform.parent.GetComponent<UbhPlayerBullet>();
+            if (playerBullet != null && playerBullet.isActive)
+            {
+                UbhObjectPool.instance.ReleaseBullet(playerBullet);
+
+                m_hp = m_hp - playerBullet.m_power;
+
+                if (m_hp <= 0)
+                {
+
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    // 적 피격모션... 필요한지?
+                }
+            }
+        }
+    }
+}
