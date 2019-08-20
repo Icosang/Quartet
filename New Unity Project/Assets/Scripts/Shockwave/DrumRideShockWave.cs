1e2c6a7c943d64495bf974974d3baebdbf4fc2e4
Hosang Yoo <you8147@naver.com>
2019-08-20 21:11:04 +0900

    라이드에서 음파나옴

---
+++ New Unity Project/Assets/Scripts/Shockwave/DrumRideShockWave.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumRideShockWave : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShockWave.Get().StartIt(transform.position, 1f, 1f, 1f, 0.2f);
+        }
+    }
+}
