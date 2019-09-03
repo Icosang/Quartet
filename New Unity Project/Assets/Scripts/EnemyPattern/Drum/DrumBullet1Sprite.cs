fa4cc839f0f35dbdd73ea056a8e9bd2cb0ebfb51
Hosang Yoo <you8147@naver.com>
2019-09-03 10:07:49 +0900

    드럼패턴1 시각적 효과 개선

---
+++ New Unity Project/Assets/Scripts/EnemyPattern/Drum/DrumBullet1Sprite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumBullet1Sprite : MonoBehaviour
+{
+    [SerializeField]
+    float randomangle = 60;
+    Vector3 random;
+
+    void Awake() {
+        random = new Vector3(0f, 0f, ((Random.Range(0f, randomangle) - (randomangle / 2f))));
+    }
+
+    void Update()
+    {
+
+        transform.Rotate(random * Time.deltaTime);
+    }
+}
