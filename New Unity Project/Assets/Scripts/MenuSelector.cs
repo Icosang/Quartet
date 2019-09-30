9f2bb2d850dd96397e4fde21d50c4aea77a9d7d4
Hosang Yoo <you8147@naver.com>
2019-09-30 17:35:59 +0900

    메뉴 스크립트 작성, 애니메이션 예제

---
+++ New Unity Project/Assets/Scripts/MenuSelector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSelector : MonoBehaviour
+{
+    GameObject[] menu;
+    int index = 0;
+    float delayTimer = 0;
+    float delayTime = 0.2f;
+    void Awake()
+    {
+        menu = GameObject.FindGameObjectsWithTag("Menu");
+        transform.position = (menu[0].transform.position) - new Vector3(400, -30, 0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow) && (delayTimer <= 0))
+        {
+            index++;
+            delayTimer = delayTime;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) && (delayTimer <= 0))
+        {
+            index--;
+            delayTimer = delayTime;
+        }
+
+        if (index < 0) index = menu.Length - 1;
+
+        if (index == menu.Length) index = 0;
+            transform.position = (menu[index].transform.position) - new Vector3(400, -30, 0);
+                if (delayTimer > 0) delayTimer -= Time.deltaTime;
+
+    }
+}
