2d0c8d32abec5a65fc79ffdcdbf4cf80aca59d80
Hosang Yoo <you8147@naver.com>
2019-10-05 17:02:31 +0900

    드럼 배경 배치,  게임매니저 생성 및 배경 움직이기 진행중

---
+++ New Unity Project/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    // Scroll main texture based on time
+    GameObject manager;
+    [SerializeField]
+    float scrollSpeed = 0.5f;
+    Renderer rend;
+    void Awake() { 
+        manager = GameObject.FindWithTag("GameManager");
+    }
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
