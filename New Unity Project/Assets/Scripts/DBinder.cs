55f3f6a92350d136cd589d76bc7d97e75019b8da
Hosang Yoo <you8147@naver.com>
2019-10-08 18:44:34 +0900

    게임매니저 예시스크립트 추가

---
+++ New Unity Project/Assets/Scripts/DBinder.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class DBinder : MonoBehaviour
+{
+    [SerializeField] MonoBehaviour[] bindTargets = null;
+
+    private bool bindingsAdded;
+
+    void OnValidate()
+    {
+        foreach (var target in bindTargets)
+        {
+            if (target == null)
+            {
+                Debug.Log("There is a null bind target.");
+            }
+        }
+    }
+
+    public void AddBindings()
+    {
+        if (bindingsAdded)
+        {
+            return;
+        }
+        bindingsAdded = true;
+        foreach (var target in bindTargets)
+        {
+            D.AddBinding(target);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var target in bindTargets)
+        {
+            D.RemoveBinding(target);
+        }
+    }
+}
