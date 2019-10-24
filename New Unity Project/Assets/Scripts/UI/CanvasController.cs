4665a546e1d065db92fb32d289a40516dce0fb4b
Hosang Yoo <you8147@naver.com>
2019-10-24 16:19:25 +0900

    UI D있는곳으로 옮김

---
+++ New Unity Project/Assets/Scripts/UI/CanvasController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    public void ActiveUI(int uinum)
+    {
+        transform.GetChild(uinum).gameObject.SetActive(true);
+    }
+}
--- New Unity Project/Assets/Scripts/UI/GMloader.cs
+++ New Unity Project/Assets/Scripts/UI/GMloader.cs
@@ -2,9 +2,19 @@
 using UnityEngine.SceneManagement;
 
 public class GMloader : MonoBehaviour
-{    private void Awake()
+{
+    private void Awake()
     {
-        if (D.Get<GameManager>() == null)
+        if (D.TryGet<GameManager>() == null)
+        {
             SceneManager.LoadScene("GameManager", LoadSceneMode.Additive);
+        }        
+    }
+    private void Start()
+    {
+        if (gameObject.tag == "PlayScreen")
+        {
+            D.Get<CanvasController>().ActiveUI(0);
+        }
     }
 }
