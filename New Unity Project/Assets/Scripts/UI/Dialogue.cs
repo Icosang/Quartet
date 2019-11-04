fe792b9be3dadc4e8d6edb8181add8405ef7a662
Hosang Yoo <you8147@naver.com>
2019-11-04 19:48:04 +0900

    대화 다이얼로그 밑작업

---
+++ New Unity Project/Assets/Scripts/UI/Dialogue.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+public class Dialogue : MonoBehaviour
+{
+    [TextArea(1, 2)]
+    public string[] sentences;
+    public Sprite[] sprites;
+    public Sprite[] dialogueWindows;
+}
---
+++ New Unity Project/Assets/Scripts/UI/TestDialogue.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TestDialogue : MonoBehaviour
+{
+    [SerializeField]
+    public Dialogue dialogue;
+
+    private DialogueManager DM;
+    private void Start()
+    {
+        DM = FindObjectOfType<DialogueManager>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X)) {
+            DM.ShowDialogue(dialogue);
+        }
+    }
+}
