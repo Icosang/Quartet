e0c5075379b95394ed15264ea2da2a08d192788c
Hosang Yoo <you8147@naver.com>
2019-11-06 21:36:46 +0900

    메인메뉴 작업

---
+++ New Unity Project/Assets/Scripts/Roller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Roller : MonoBehaviour
+{
+
+    // Scroll main texture based on time
+    Renderer rend;
+    [SerializeField]
+    float mag = -1.0f;
+    [SerializeField]
+    float startposition = 0f;
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * mag + startposition;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
